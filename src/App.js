70f12df1a83a730212c48ac8cdaf76605fb7fb6c
Carlos Carniato <carloscarniato@gmail.com>
2020-12-03 09:29:04 -0300

    change default page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import pokeball from './pokeball.png';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/login"}>Poke Store</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/login"}>Entrar</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Registrar</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
         <img src={pokeball} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </div>
+      </div>
+    </div>  
   );
 }
 
